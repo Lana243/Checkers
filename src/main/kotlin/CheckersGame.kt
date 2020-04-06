365d711bfb595536f1255af2a7dcb73dc41262fa
DenisKorotchenko <dzd1024ozd@gmail.com>
2020-04-06 19:57:21 +0300

    base console UI player

---
+++ src/main/kotlin/CheckersGame.kt
@@ -0,0 +1,21 @@
+class CheckersGame (private val model : CheckersModel, playerWhite : BasePlayer, playerBlack : BasePlayer) {
+    private val players = Array<BasePlayer>(2) { i -> if (i == 0) playerWhite else playerBlack}
+    private var whoMoves = 0 //0 - white, 1 - black
+
+    fun game() {
+        while (true) {
+            model.printBoardOnConsole()
+            val turn = players[whoMoves].makeTurn(model)
+            if (turn == null) {
+                println("Invalid format. Please, try again")
+            } else {
+                if (!model.canMove(turn)) {
+                    println("Illegal turn. Please, try again")
+                } else {
+                    model.move(turn)
+                    whoMoves = (whoMoves + 1) % 2
+                }
+            }
+        }
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,4 @@
 fun main(args: Array<String>){
-    val checkersModel = CheckersModel()
-    checkersModel.printBoardOnConsole()
-    checkersModel.move(BaseTurn(Color.WHITE, Pair(2, 2), Pair(3, 3)))
-    checkersModel.printBoardOnConsole()
-    checkersModel.move(BaseTurn(Color.BLACK, Pair(5, 5), Pair(4, 4)))
-    checkersModel.printBoardOnConsole()
-    checkersModel.move(BaseTurn(Color.WHITE, Pair(3, 3), Pair(4, 4)))
-    checkersModel.printBoardOnConsole()
+    val game = CheckersGame(CheckersModel(), ConsoleUIPlayer("White Player", Color.WHITE), ConsoleUIPlayer("Black Player", Color.BLACK))
+    game.game()
 }
