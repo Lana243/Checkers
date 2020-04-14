8bc079c5884f34c0ab3c6c173412c8c671dfb7c4
DenisKorotchenko <dzd1024ozd@gmail.com>
2020-04-14 22:35:35 +0300

    Change BaseBoard type from abstract class to interface
    Move 'board' and 'boardSize' properties from
     BaseBoard to CheckersBoard.
     So change BaseBoard type to interface.
    
    Also change board array type from 2-dimensional
     to 'flat' array.
    
    Add some files to .gitignore

--- src/main/kotlin/BaseBoard.kt
+++ src/main/kotlin/BaseBoard.kt
@@ -1,12 +1,10 @@
-abstract class BaseBoard(val boardSize: Int) {
+interface BaseBoard {
 
-    val board = Array(boardSize) {i -> Array(boardSize) {j -> Square(if ((i + j) % 2 == 0) -1 else 1)} }
+    fun setStartPosition()
+    operator fun get(i: Int, j: Int) : Square
+    operator fun set(i: Int, j: Int, newSquare: Square)
+    operator fun get(coords: Pair<Int, Int>) : Square
+    operator fun set(coords: Pair<Int, Int>, newSquare: Square)
 
-    abstract fun setStartPosition()
-    abstract operator fun get(i: Int, j: Int) : Square
-    abstract operator fun set(i: Int, j: Int, newSquare: Square)
-    abstract operator fun get(coords: Pair<Int, Int>) : Square
-    abstract operator fun set(coords: Pair<Int, Int>, newSquare: Square)
-
-    abstract fun print()
+    fun print()
 }
--- src/main/kotlin/CheckersBoard.kt
+++ src/main/kotlin/CheckersBoard.kt
@@ -1,29 +1,32 @@
-class CheckersBoard(boardSize: Int) : BaseBoard(boardSize) {
+class CheckersBoard(private val boardSize: Int) : BaseBoard {
     init {
         setStartPosition()
     }
+
+    private val board = Array(boardSize * boardSize) {i -> Square(if ((i / boardSize + i % boardSize) % 2 == 0) -1 else 1)}
+
     override fun setStartPosition() {
         for (i in 0 until boardSize) {
             for (j in 0 until boardSize) {
-                if (i < 3 && board[i][j].color == -1) {
-                    board[i][j].figure = Figure(1)
+                if (i < 3 && board[i * boardSize + j].color == -1) {
+                    board[i * boardSize + j].figure = Figure(1)
                 }
-                if (i == 3 || i == 4 || board[i][j].color == 1) {
-                    board[i][j].figure = null
+                if (i == 3 || i == 4 || board[i * boardSize + j].color == 1) {
+                    board[i * boardSize + j].figure = null
                 }
-                if (i >= 5 && board[i][j].color == -1) {
-                    board[i][j].figure = Figure(-1)
+                if (i >= 5 && board[i * boardSize + j].color == -1) {
+                    board[i * boardSize + j].figure = Figure(-1)
                 }
             }
         }
     }
 
     override operator fun get(i: Int, j: Int) : Square {
-        return board[i][j]
+        return board[i * boardSize + j]
     }
 
     override operator fun get(coords: Pair<Int, Int>): Square {
-        return board[coords.first][coords.second]
+        return board[coords.first * boardSize + coords.second]
     }
 
     override operator fun set(i: Int, j: Int, newSquare: Square) {
@@ -31,22 +34,22 @@ class CheckersBoard(boardSize: Int) : BaseBoard(boardSize) {
     }
 
     override operator fun set(coords: Pair<Int, Int>, newSquare: Square) {
-        board[coords.first][coords.second] = newSquare
+        board[coords.first * boardSize + coords.second] = newSquare
     }
 
     override fun print() {
         for (i in boardSize - 1 downTo 0) {
             for (j in 0 until boardSize) {
                 var t : String
-                if (board[i][j].figure == null) {
+                if (board[i * boardSize + j].figure == null) {
                     t = "."
                 } else {
-                    t = if (board[i][j].figure!!.color == 1) {
+                    t = if (board[i * boardSize + j].figure!!.color == 1) {
                         "w"
                     } else {
                         "b"
                     }
-                    if (board[i][j].figure!!.type == FigureType.Queen) {
+                    if (board[i * boardSize + j].figure!!.type == FigureType.Queen) {
                         t = t.toUpperCase()
                     }
                 }
@@ -56,6 +59,4 @@ class CheckersBoard(boardSize: Int) : BaseBoard(boardSize) {
         }
         println()
     }
-
-
 }
