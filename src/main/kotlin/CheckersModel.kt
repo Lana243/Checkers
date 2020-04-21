baf81503fda8640070e3f8be565d782067db6cc4
Svetlana Shmidt <svetik.shmidt.2001@mail.ru>
2020-04-21 17:57:40 +0500

    Fix eating
    Add ban to eat one checker twice

--- src/main/kotlin/CheckersModel.kt
+++ src/main/kotlin/CheckersModel.kt
@@ -10,6 +10,7 @@ class CheckersModel(val board : CheckersBoard = CheckersBoard(8)) : BaseModel() 
     }
 
     var eatingChecker : Square? = null
+    private var eatenList = ArrayList<Square>()
     /* This functions checking turn for legacy and rules and also find checker, that will be eaten.
     Its return value has 3 options:
         if turn is illegal, it returns null,
@@ -57,6 +58,11 @@ class CheckersModel(val board : CheckersBoard = CheckersBoard(8)) : BaseModel() 
             for (d in 1 until abs(verticals)) {
                 val x = turn.from.first + horizontals.sign * d
                 val y = turn.from.second + verticals.sign * d
+
+                if (board[x, y].eaten) {
+                    //square contained eaten checker
+                    return null
+                }
                 if (board[x, y].figure != null) {
                     if (board[x, y].figure?.color == whoMoves) {
                         //The checker between 'from' and 'to' squares has same color with moving player
@@ -111,7 +117,7 @@ class CheckersModel(val board : CheckersBoard = CheckersBoard(8)) : BaseModel() 
         return false
     }
 
-    fun canEat(squareFromCoords : Pair<Int, Int>): Boolean {
+    private fun canEat(squareFromCoords : Pair<Int, Int>): Boolean {
         val (i, j) = squareFromCoords
         if (board[i, j].figure?.type == FigureType.Ordinary) {
             for ((di, dj) in listOf(-1 to -1, 1 to -1, 1 to 1, -1 to 1)) {
@@ -150,9 +156,20 @@ class CheckersModel(val board : CheckersBoard = CheckersBoard(8)) : BaseModel() 
             board[turn.to].figure!!.type = FigureType.Queen
     }
 
+    private fun updateEatenList() {
+        for (i in 0..eatenList.size) {
+            eatenList[i].eaten = false
+        }
+        eatenList.clear()
+    }
+
     override fun move(turn: BaseTurn)  {
         val canMoveResult = canMove(turn) ?: throw IllegalStateException(
                 "Turn isn't valid now, but model is trying to apply it.")
+        if (canMoveResult !== board[turn.from]) {
+            canMoveResult.eaten = true
+            eatenList.add(canMoveResult)
+        }
         board[turn.to].figure = board[turn.from].figure
         board[turn.from].figure = null
         canMoveResult.figure = null
@@ -160,6 +177,7 @@ class CheckersModel(val board : CheckersBoard = CheckersBoard(8)) : BaseModel() 
         if (canMoveResult === board[turn.from] || !canEat(turn.to)) {
             whoMoves = whoMoves.nextColor()
             eatingChecker = null
+            updateEatenList()
         } else {
             eatingChecker = board[turn.to]
         }
--- src/main/kotlin/Square.kt
+++ src/main/kotlin/Square.kt
@@ -1,21 +1,25 @@
 class Square(val color: Int) {
     var figure : Figure? = null
+    var eaten = false;
 
     constructor(square: Square) : this(square.color) {
         figure = if (square.figure == null) null else Figure(square.figure!!)
+        eaten = square.eaten
     }
-
-
 
     override fun equals(other: Any?): Boolean {
         return if (other is Square) {
-            val oFigure = other.figure
+            if (this.eaten != other.eaten)
+                false
+            else {
+                val oFigure = other.figure
 
-            val tFigure = this.figure
-            if (oFigure != null && tFigure != null) {
-                oFigure.type == tFigure.type && oFigure.color == tFigure.color
-            } else {
-                oFigure == null && tFigure == null
+                val tFigure = this.figure
+                if (oFigure != null && tFigure != null) {
+                    oFigure.type == tFigure.type && oFigure.color == tFigure.color
+                } else {
+                    oFigure == null && tFigure == null
+                }
             }
         } else {
             false
