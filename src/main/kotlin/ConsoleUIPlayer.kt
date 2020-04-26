a74ac45b69f6eacff6a2dd2c045e1e9babe0d737
DenisKorotchenko <dzd1024ozd@gmail.com>
2020-04-26 23:38:16 +0300

    Add input type enum

--- src/main/kotlin/ConsoleUIPlayer.kt
+++ src/main/kotlin/ConsoleUIPlayer.kt
@@ -1,6 +1,11 @@
+enum class InputType {
+    INDEXING_0,
+    STANDARD_CHESSBOARD_NOTATION
+}
+
 class ConsoleUIPlayer(private val name : String,
                       col : Color,
-                      private val inputType: Int = 0) : BasePlayer(col) {
+                      private val inputType: InputType = InputType.STANDARD_CHESSBOARD_NOTATION) : BasePlayer(col) {
     override fun makeTurn(model : BaseModel): BaseTurn {
         println("Player $name - it's your turn.")
         while (true) {
@@ -15,12 +20,12 @@ class ConsoleUIPlayer(private val name : String,
 
     private fun readTurn(): BaseTurn {
         when (inputType) {
-            0 -> {
+            InputType.INDEXING_0 -> {
                 val input = readLine() ?: ""
                 val (x1, y1, x2, y2) = input.split(' ')
                 return BaseTurn(color, Pair(x1.toInt(), y1.toInt()), Pair(x2.toInt(), y2.toInt()))
             }
-            1 -> {
+            InputType.STANDARD_CHESSBOARD_NOTATION -> {
                 val input = (readLine() ?: "").split(Regex("""\s+"""))
                 val x1 = input[0][0].toInt() - 'a'.toInt()
                 val y1 = input[0][1].toInt() - '1'.toInt()
